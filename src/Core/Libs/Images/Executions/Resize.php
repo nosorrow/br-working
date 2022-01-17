627ff2621d2729bba9cc9550c744480137e38775
plamenorama <plamenorama@gmail.com>
2022-01-17 20:12:11 +0200

    2022-01-17 fix resize image for php 8.1

--- src/Core/Libs/Images/Executions/Resize.php
+++ src/Core/Libs/Images/Executions/Resize.php
@@ -7,24 +7,29 @@ use Core\Libs\Images\Image as Image;
 class Resize extends AbstractExecutions
 {
 
-    public function execute(Image $image)
+	/**
+	 * @throws \Exception
+	 */
+	public function execute(Image $image)
     {
-        if (count($this->arguments) > 2) {
-            throw new \Exception("Too many arguments");
+		if (count($this->arguments) > 2) {
+			throw new \Exception("Too many arguments");
 
-        } elseif (count($this->arguments) == 2) {
-            list($width, $height) = $this->arguments;
+		}
 
-        } elseif (count($this->arguments) == 1){
-            $width = $this->arguments[0];
-            $height = null;
+		if (count($this->arguments) === 2) {
+			[$width, $height] = $this->arguments;
 
-        } else {
-            throw new \Exception("No arguments !");
+		} elseif (count($this->arguments) === 1){
+			$width = $this->arguments[0];
+			$height = null;
 
-        }
+		} else {
+			throw new \Exception("No arguments !");
 
-        list($width_orig, $height_orig) = $image->src_image_info;
+		}
+
+		[$width_orig, $height_orig] = $image->src_image_info;
 
         return $this->resize($image->src_image, $width, $height, $width_orig, $height_orig);
     }
